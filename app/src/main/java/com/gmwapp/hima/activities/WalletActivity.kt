01c56f247afaace0be516a548a2cbe2c302038ab
Rishabh Kumar <rishabh845401@gmail.com>
2025-05-19 21:34:16 +0530

    Phone pe integrated in wallet activity

--- app/src/main/java/com/gmwapp/hima/activities/WalletActivity.kt
+++ app/src/main/java/com/gmwapp/hima/activities/WalletActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -28,8 +29,21 @@ import com.gmwapp.hima.viewmodels.ProfileViewModel
 import com.gmwapp.hima.viewmodels.UpiPaymentViewModel
 import com.gmwapp.hima.viewmodels.UpiViewModel
 import com.gmwapp.hima.viewmodels.WalletViewModel
+import com.phonepe.intent.sdk.api.PhonePeInitException
+import com.phonepe.intent.sdk.api.PhonePeKt
+import com.phonepe.intent.sdk.api.models.PhonePeEnvironment
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.Callback
+import okhttp3.FormBody
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import org.json.JSONObject
 import retrofit2.Call
+import java.io.IOException
 
 @AndroidEntryPoint
 class WalletActivity : BaseActivity()  {
@@ -61,6 +75,27 @@ class WalletActivity : BaseActivity()  {
     var pointsId = ""
     var paymentGateway = ""
 
+    private var lastOrderId: String = ""
+    private var isPhonePeInitialized = false
+
+
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val statusCode = result.resultCode
+        val status = result.data?.getStringExtra("status") ?: "UNKNOWN"
+        Log.d("PhonePe", "SDK resultCode: $statusCode, status: $status")
+
+        if (lastOrderId.isNotEmpty()) {
+            checkOrderStatus(lastOrderId)
+        }
+
+        if (statusCode == RESULT_OK) {
+           // Toast.makeText(this, "Payment Successful", Toast.LENGTH_LONG).show()
+        } else {
+          //  Toast.makeText(this, "Payment Failed or Cancelled", Toast.LENGTH_LONG).show()
+        }
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +110,7 @@ class WalletActivity : BaseActivity()  {
         }
         initUI()
         observeCoins()
+        intializePhonpe()
     }
 
     override fun onResume() {
@@ -82,6 +118,142 @@ class WalletActivity : BaseActivity()  {
         val userData = BaseApplication.getInstance()?.getPrefs()?.getUserData()
         userData?.id?.let { profileViewModel.getUsers(it) }
     }
+
+    fun intializePhonpe(){
+
+        val isInitialized = PhonePeKt.init(
+            context = this,
+            merchantId = "SU2505161111008337542920", // Replace in PROD
+            flowId = "user001",
+            phonePeEnvironment = PhonePeEnvironment.RELEASE, // Use RELEASE in prod
+            enableLogging = true,
+            appId = null
+        )
+
+        if (isInitialized) {
+            isPhonePeInitialized = true
+        } else {
+            Log.e("PhonePe", "SDK Initialization Failed")
+            Toast.makeText(this, "PhonePe SDK init failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun fetchOrderFromBackend(coinId: String) {
+        val client = OkHttpClient()
+
+        val formBody = FormBody.Builder()
+            .add("coins_id", coinId)
+            .build()
+
+        Log.d("SelectedCoinID", " $coinId")
+
+        val request = Request.Builder()
+            .url("https://himaapp.in/api/phonepe/live/create-order") // Should return { token, orderId }
+            .post(formBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this@WalletActivity, "API Failure: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                val responseStr = response.body?.string() ?: return
+                Log.d("PhonePeResponse", "Backend Response: $responseStr")
+
+                try {
+                    val json = JSONObject(responseStr)
+                    val token = json.getString("token")
+                    val orderId = json.getString("orderId")
+
+                    lastOrderId = orderId
+
+                    runOnUiThread {
+                        startPhonePeCheckout(orderId, token)
+                    }
+
+                } catch (e: Exception) {
+                    runOnUiThread {
+                        Toast.makeText(this@WalletActivity, "Invalid server response", Toast.LENGTH_SHORT).show()
+                        Log.d("PhonpeException","$e")
+                    }
+                }
+            }
+        })
+    }
+
+    private fun startPhonePeCheckout(orderId: String, token: String) {
+        if (!isAnyUPIAppInstalled()) {
+            Toast.makeText(this, "No UPI app installed", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        try {
+            PhonePeKt.startCheckoutPage(
+                context = this,
+                token = token,
+                orderId = orderId,
+                activityResultLauncher = activityResultLauncher
+            )
+        } catch (e: PhonePeInitException) {
+            Log.e("PhonePe", "Checkout Failed: ${e.message}")
+            Toast.makeText(this, "Could not start payment", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkOrderStatus(orderId: String) {
+        val client = OkHttpClient()
+
+        val json = """{ "orderId": "$orderId" }"""
+        val mediaType = "application/json".toMediaTypeOrNull()
+        val body = RequestBody.create(mediaType, json)
+
+        val request = Request.Builder()
+            .url("https://himaapp.in/api/phonepe/live/check-status")
+            .post(body) // ✅ Correct method
+            .addHeader("Content-Type", "application/json")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(this@WalletActivity, "Status check failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: Response) {
+                val resultStr = response.body?.string()
+                val json = JSONObject(resultStr)
+                val state = json.getString("state")
+                Log.d("PhonePeOrderStatus", "Order Status: $resultStr")
+                Log.d("PhonePeOrderState", "Order State: $state")
+
+
+                if (state=="SUCCESS"){
+                    runOnUiThread{
+                        Toast.makeText(this@WalletActivity, "Coin Added Successfully", Toast.LENGTH_LONG).show()
+                    }
+
+                }else{
+                    runOnUiThread{
+                        Toast.makeText(this@WalletActivity, "Payment Failed", Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
+        })
+    }
+
+    private fun isAnyUPIAppInstalled(): Boolean {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse("upi://pay")
+        val pm = packageManager
+        val activities = pm.queryIntentActivities(intent, 0)
+        return activities.isNotEmpty()
+    }
+
 
     fun observeCoins(){
         profileViewModel.getUserLiveData.observe(this, Observer {
@@ -214,79 +386,82 @@ class WalletActivity : BaseActivity()  {
             if (userId != null && pointsId.isNotEmpty()) {
                 if (pointsIdInt != null) {
 
-
-                    if (paymentGateway.isNotEmpty()) {
-
-                        when (paymentGateway) {
-                            "gpay" -> {
-
-                                // ✅ Save userId and pointsIdInt BEFORE launching billing
-                                val preferences = DPreferences(this)
-                                preferences.setSelectedUserId(userId.toString())
-                                preferences.setSelectedPlanId(java.lang.String.valueOf(pointsIdInt))
-                                billingManager!!.purchaseProduct(
-                                    //"coin_14",
-                                    pointsId,
-                                    userId,
-                                    pointsIdInt
-                                )
-                                WalletViewModel.navigateToMain.observe(this, Observer { shouldNavigate ->
-
-                                    if (shouldNavigate) {
-                                        Toast.makeText(
-                                            this,
-                                            "Coin purchased successfully",
-                                            Toast.LENGTH_SHORT
-                                        )
-                                            .show()
-                                        userData?.id?.let { profileViewModel.getUsers(it) }
-
-                                        profileViewModel.getUserLiveData.observe(this, Observer {
-                                            it.data?.let { it1 ->
-                                                BaseApplication.getInstance()?.getPrefs()
-                                                    ?.setUserData(it1)
-                                            }
-                                            binding.tvCoins.text = it.data?.coins.toString()
-                                            WalletViewModel._navigateToMain.postValue(false)
-                                        })
-                                    } else {
-
-                                        profileViewModel.getUserLiveData.observe(this, Observer {
-                                            it.data?.let { it1 ->
-                                                BaseApplication.getInstance()?.getPrefs()
-                                                    ?.setUserData(it1)
-                                            }
-                                            binding.tvCoins.text = it.data?.coins.toString()
-
-                                        })
-                                    }
-                                })
-                                }
-
-                            "upigateway" -> {
-
-                                Log.d("upigateway","Clicked")
-                                val userData = BaseApplication.getInstance()?.getPrefs()?.getUserData()
-                                var userid = userData?.id
-                                userid?.let {
-                                    val clientTxnId = generateRandomTxnId(
-                                        it,
-                                        pointsId
-                                    )  // Generate a new transaction ID
-                                    upiPaymentViewModel.createUpiPayment(it, clientTxnId, total_amount)
-                                }
-
-                            }
-
-
-                            else -> {
-                                Toast.makeText(this, "Invalid Payment Gateway", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-
-
-                        }
+                    if (isPhonePeInitialized){
+                        fetchOrderFromBackend(pointsId)
                     }
+
+//                    if (paymentGateway.isNotEmpty()) {
+//
+//                        when (paymentGateway) {
+//                            "gpay" -> {
+//
+//                                // ✅ Save userId and pointsIdInt BEFORE launching billing
+//                                val preferences = DPreferences(this)
+//                                preferences.setSelectedUserId(userId.toString())
+//                                preferences.setSelectedPlanId(java.lang.String.valueOf(pointsIdInt))
+//                                billingManager!!.purchaseProduct(
+//                                    //"coin_14",
+//                                    pointsId,
+//                                    userId,
+//                                    pointsIdInt
+//                                )
+//                                WalletViewModel.navigateToMain.observe(this, Observer { shouldNavigate ->
+//
+//                                    if (shouldNavigate) {
+//                                        Toast.makeText(
+//                                            this,
+//                                            "Coin purchased successfully",
+//                                            Toast.LENGTH_SHORT
+//                                        )
+//                                            .show()
+//                                        userData?.id?.let { profileViewModel.getUsers(it) }
+//
+//                                        profileViewModel.getUserLiveData.observe(this, Observer {
+//                                            it.data?.let { it1 ->
+//                                                BaseApplication.getInstance()?.getPrefs()
+//                                                    ?.setUserData(it1)
+//                                            }
+//                                            binding.tvCoins.text = it.data?.coins.toString()
+//                                            WalletViewModel._navigateToMain.postValue(false)
+//                                        })
+//                                    } else {
+//
+//                                        profileViewModel.getUserLiveData.observe(this, Observer {
+//                                            it.data?.let { it1 ->
+//                                                BaseApplication.getInstance()?.getPrefs()
+//                                                    ?.setUserData(it1)
+//                                            }
+//                                            binding.tvCoins.text = it.data?.coins.toString()
+//
+//                                        })
+//                                    }
+//                                })
+//                                }
+//
+//                            "upigateway" -> {
+//
+//                                Log.d("upigateway","Clicked")
+//                                val userData = BaseApplication.getInstance()?.getPrefs()?.getUserData()
+//                                var userid = userData?.id
+//                                userid?.let {
+//                                    val clientTxnId = generateRandomTxnId(
+//                                        it,
+//                                        pointsId
+//                                    )  // Generate a new transaction ID
+//                                    upiPaymentViewModel.createUpiPayment(it, clientTxnId, total_amount)
+//                                }
+//
+//                            }
+//
+//
+//                            else -> {
+//                                Toast.makeText(this, "Invalid Payment Gateway", Toast.LENGTH_SHORT)
+//                                    .show()
+//                            }
+//
+//
+//                        }
+//                    }
                 }
             } else {
                 Toast.makeText(this, "Invalid input data", Toast.LENGTH_SHORT).show()
