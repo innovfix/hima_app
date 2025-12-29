ae205188d20e825b46b14686464f610baf457a49
rishabh2828 <rishabh845401@gmail.com>
2025-12-29 17:30:59 +0530

    updated code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.gmwapp.hima"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1057
-        versionName = "1057"
+        versionCode = 2001
+        versionName = "2001"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
